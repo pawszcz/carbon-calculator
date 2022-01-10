8bf21c79141ffa8a711324f39bf8c77a73744961
Pawel <pawelszczepaniak@onet.eu>
2022-01-10 20:41:31 +0100

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home(props) {
   const calculateCarbon = async event => {
     event.preventDefault();
 
+    console.log();
     setLoading(true);
 
     const url = 'api/calculator';
